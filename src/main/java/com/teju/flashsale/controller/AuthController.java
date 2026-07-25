a4f65b0a9fca5b3ad12fc4f54809002147c39b3b
tejuingalagi <tejuingalagi07@gmail.com>
2026-07-25 15:59:19 +0530

    Add authentication controller

---
+++ src/main/java/com/teju/flashsale/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.teju.flashsale.controller;
+
+import com.teju.flashsale.dto.AuthResponse;
+import com.teju.flashsale.dto.LoginRequest;
+import com.teju.flashsale.dto.SignupRequest;
+import com.teju.flashsale.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(@RequestBody SignupRequest request) {
+        return new ResponseEntity<>(authService.signup(request), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return new ResponseEntity<>(authService.login(request), HttpStatus.OK);
+    }
+}
